39c6b562bcac635b9ff417b4072d2ef57bc3cfb2
adegokei <adegokei@uni.coventry.ac.uk>
2021-09-18 00:59:21 +0500

    updated

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,20 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose=require('mongoose')
 
-const connectDB = async () => {
-    try{
-        // mongodb connection string
-        const con = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false,
-            useCreateIndex: true
-        })
+const connectDB=async()=>{
 
-        console.log(`MongoDB connected : ${con.connection.host}`);
-    }catch(err){
-        console.log(err);
-        process.exit(1);
-    }
+try{ //mongodb connection string
+    const con=await mongoose.connect(process.env.MONGO_URL,{
+        useNewUrlParser: true, 
+
+useUnifiedTopology: true 
+       
+    })
+    console.log(`MongoDB connected :${con.connection.host}`)
+
+}catch(err){
+console.log(err)
+process.exit(1)
 }
 
-module.exports = connectDB
+
+}
+
+
+module.exports=connectDB
+
+
+
+
+
+
+
